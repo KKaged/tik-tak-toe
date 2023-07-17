9be057710b704531a1f310fa3ba301f53efa2db8
KKaged <dhigaredaleon707@gmail.com>
2023-07-17 12:12:18 +0900

    created a player factory.

---
+++ index.js
@@ -0,0 +1,9 @@
+const gameBoard = (() => {
+  const playerFact = (player, sign) => {
+    return { player, sign };
+  };
+  return playerFact;
+})();
+
+const player1 = gameBoard("Player 1", "X");
+const player2 = gameBoard("Player 2", "O");
