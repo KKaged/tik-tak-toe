b4882efcea8a68db6f29df4628efa3cdee07ab44
KKaged <dhigaredaleon707@gmail.com>
2023-07-18 19:40:43 +0900

    whenever a button is pressed, it now works.

--- index.js
+++ index.js
@@ -7,3 +7,15 @@ const gameBoard = (() => {
 
 const player1 = gameBoard("Player 1", "X");
 const player2 = gameBoard("Player 2", "O");
+
+const who = document.getElementsByClassName("current-player");
+const cards = document.querySelectorAll(".card");
+
+const currentPlayer = () => {
+  cards.forEach((card) => {
+    card.addEventListener("click", function () {
+      console.log(`Test`);
+    });
+  });
+};
+currentPlayer();
