f19462e0286ee22a594c41597a1fc43e0a53acba
KKaged <dhigaredaleon707@gmail.com>
2023-07-19 20:51:39 +0900

    Can input X and O.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const cards = document.querySelectorAll(".card");
+
 const gameBoard = (() => {
   const playerFact = (player, sign) => {
     return { player, sign };
@@ -8,14 +10,21 @@ const gameBoard = (() => {
 const player1 = gameBoard("Player 1", "X");
 const player2 = gameBoard("Player 2", "O");
 
+let currentPlayer = player1;
+
 const game = () => {
-  const cards = document.querySelectorAll(".card");
+  let currentPlayer = player1; // Set the initial player
+
   cards.forEach((card) => {
     card.addEventListener("click", function () {
+      // Switch players
+      currentPlayer = currentPlayer === player1 ? player2 : player1;
+
       const pick = document.createElement("div");
       pick.classList.add("pick");
-      pick.textContent = player1.sign;
+      pick.textContent = currentPlayer.sign;
       card.appendChild(pick);
+      // Call a function or perform any necessary actions for the new player
     });
   });
 };
